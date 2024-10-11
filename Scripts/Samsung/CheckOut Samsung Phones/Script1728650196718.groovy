3b696182d51a8a865e566492d61cde4ba5232940
SulaimanSantunSejati <sulaimansantun.s@gmail.com>
2024-10-12 04:20:10 +0700

    Submit task katalon mobile test

---
+++ Scripts/Samsung/CheckOut Samsung Phones/Script1728650196718.groovy
@@ -0,0 +1,67 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+Mobile.startApplication('C:\\automate\\katalon\\Eduapp\\mobileapp\\Shop Samsung_2.0.34917_APKPure.apk', true)
+
+Mobile.delay(25, FailureHandling.STOP_ON_FAILURE)
+
+Mobile.tap(findTestObject('Samsung/Shop as a guest - Button'), 800)
+
+Mobile.delay(2, FailureHandling.STOP_ON_FAILURE)
+
+Mobile.tap(findTestObject('Samsung/Interest Phones - button'), 0)
+
+Mobile.tap(findTestObject('Samsung/continue to page - button'), 0)
+
+Mobile.delay(3, FailureHandling.STOP_ON_FAILURE)
+
+Mobile.tap(findTestObject('Object Repository/Samsung/Shop_Button'), 0)
+
+Mobile.tap(findTestObject('Object Repository/Samsung/Phones-Products'), 0)
+
+Mobile.delay(3, FailureHandling.STOP_ON_FAILURE)
+
+Mobile.tap(findTestObject('Object Repository/Samsung/Galaxy Z-product'), 0)
+
+Mobile.tap(findTestObject('Object Repository/Samsung/Choose-Galaxy Z Fold6'), 0)
+
+Mobile.scrollToText('Trade-in')
+
+Mobile.tap(findTestObject('Object Repository/Samsung/Choose-No to trade in'), 0)
+
+Mobile.scrollToText('No coverage')
+
+Mobile.tap(findTestObject('Object Repository/Samsung/Button - No coverage'), 0)
+
+Mobile.scrollToText('Delivery for')
+
+Mobile.tap(findTestObject('Object Repository/Samsung/Button - Continue'), 0)
+
+Mobile.delay(2, FailureHandling.STOP_ON_FAILURE)
+
+Mobile.tap(findTestObject('Object Repository/Samsung/Skip add-ons'), 0)
+
+Mobile.delay(2, FailureHandling.STOP_ON_FAILURE)
+
+Mobile.tap(findTestObject('Object Repository/Samsung/Add to cart-button'), 0)
+
+Mobile.tap(findTestObject('Object Repository/Samsung/checkout-button'), 0)
+
+Mobile.closeApplication()
+
---
+++ Test Suites/DuoApp.ts
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>DuoApp</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>f5d39058-76d2-4aa2-8ba8-b70d664f4ca0</testSuiteGuid>
+   <testCaseLink>
+      <guid>496b0834-ea4f-4ef9-b89d-6eaaefb8cabc</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Calculator/Choose Theme</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>82a1d5d9-c778-4c2a-aae5-56f9fbd2c9f7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Pendaki/OpenDetail</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
